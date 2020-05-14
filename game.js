f2099cb3f02f5ef3f82d638a9b4888a3185a7e16
brandon <booth.brandon4@gmail.com>
2020-05-14 17:38:30 -0400

    can now buy houses

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const randomize = () => Math.floor(Math.random());
+
 class Player {
   constructor(name, money, space) {
     this.name = name;
@@ -7,31 +9,27 @@ class Player {
     this.properties = {
       red: {
         total: 2,
+        houses: 0,
         owned: []
       },
       blue: {
         total: 3,
+        houses: 0,
         owned: []
       },
       green: {
         total: 2,
+        houses: 0,
         owned: []
       }
     };
   }
 
-  getMoney() {
-    console.log(this.money);
-    return this.money;
-  }
-
   getSpace() {
-    // console.log(this.space);
     return this.space;
   }
 
   moveSpace(roll, board) {
-    // console.log({roll, space: this.space})
     const current = this.space + roll;
     if (current > board.length-1) {
       this.space = current - board.length;
@@ -39,40 +37,57 @@ class Player {
     } else {
       this.space = current;
     };
-    // console.log('Space', this.space);
   }
 
   purchaseProperty(property) {
-    // console.log('Purchasing', this.money > property.cost);
     if (property.owner === null) {
       if (this.money > property.cost) {
         this.money -= property.cost;
         this.properties[property.type].owned.push(property);
       } else {
-        // console.log('You do not have enough money');
+        console.log('You do not have enough money');
       }
     }
+    console.log('PURCHASE', property.cost, this.money);
   }
 
-  payOtherPlayer(value) {
-    this.money += value;
-    if (this.money <= 0) {
-      // console.log('You are bankrupt!');
-      this.bankrupt = true;
+  buyHouses() {
+    const randomized = [];
+    const options = Object.keys(this.properties);
+    const num = randomize();
+    options.forEach(e => {
+      num === 1 ? randomized.push(this.properties[e]) : randomized.unshift(this.properties[e]);
+    });
+    const selection = randomized.find(e => e.total === e.owned.length && e.houses < 4);
+    console.log('HOUSES', selection);
+    if (selection) {
+      this.money -= selection.owned[0].houseCost;
+      console.log('TYPE', selection.owned[0].type);
+      this.properties[selection.owned[0].type].houses += 1;
     }
   }
 
-  getPaid(value) {
-    this.money += value;
+  payOtherPlayer(type, value) {
+    this.money -= this.properties[type].houses * value;
+    if (this.money <= 0) {
+      console.log('You are bankrupt!');
+      this.bankrupt = true;
+    }
+    console.log('PAYING', this.money);
+  }
+
+  getPaid(type, value) {
+    this.money += this.properties[type].houses * value;
+    console.log('PAID', this.money);
   }
 
   payTaxes() {
-    this.money -= this.money*0.5;
+    this.money -= Math.floor(this.money*0.5);
   }
 
   passGo() {
-    // console.log('PASSED GO');
-    this.money = this.money + 200;
+    console.log('PASSED GO');
+    this.money = this.money + 200
   }
 }
 
@@ -84,55 +99,61 @@ class Properties {
         name: 'red1',
         cost: 100,
         payout: 50,
-        owner: null
+        owner: null,
+        houseCost: 50
       },
       red2: {
         type: 'red',
         name: 'red2',
         cost: 125,
         payout: 75,
-        owner: null
+        owner: null,
+        houseCost: 50
       },
       blue1: {
         type: 'blue',
         name: 'blue1',
         cost: 200,
         payout: 100,
-        owner: null
+        owner: null,
+        houseCost: 100
       },
       blue2: {
         type: 'blue',
         name: 'blue2',
         cost: 225,
         payout: 125,
-        owner: null
+        owner: null,
+        houseCost: 100
       },
       blue3: {
         type: 'blue',
         name: 'blue3',
         cost: 250,
         payout: 150,
-        owner: null
+        owner: null,
+        houseCost: 150
       },
       green1: {
         type: 'green',
         name: 'green1',
         cost: 300,
         payout: 150,
-        owner: null
+        owner: null,
+        houseCost: 150
       },
       green2: {
         type: 'green',
         name: 'green2',
         cost: 350,
         payout: 175,
-        owner: null
+        owner: null,
+        houseCost: 150
       }
     }
   }
 
   checkProperties(property) {
-    // console.log(property, this.properties[property]);
     return this.properties[property];
   }
 
--- run.js
+++ run.js
@@ -7,7 +7,7 @@ const Player2 = new Player('Amanda', 1000, 0);
 const Player3 = new Player('Blaire', 1000, 0);
 const AllProperties = new Properties();
 
-const rollDice = () => Math.ceil(Math.random() * 4)
+const rollDice = () => Math.ceil(Math.random() * 4);
 
 const playGame = (players, props) => {
   let total = 0;
@@ -15,25 +15,29 @@ const playGame = (players, props) => {
   
   while(players.filter(e => !e.bankrupt).length > 1) {
     const currentPlayer = players[current];
-    console.log(`Player ${current+1}'s turn`);
-    const dice = rollDice();
-    currentPlayer.moveSpace(dice, board);
-    const space = board[currentPlayer.getSpace()];
-    if (space === 'go') {
-      currentPlayer.passGo();
-    } else if (space === 'taxes') {
-      currentPlayer.payTaxes();
-    } else {
-      const property = props.checkProperties(space);
-      // console.log({property});
-      if (property.owner === null) {
-        currentPlayer.purchaseProperty(property);
-        props.assignOwner(currentPlayer.name, property.name);
+    if (!currentPlayer.bankrupt) {
+      console.log(`Player ${current+1}'s turn`);
+      const dice = rollDice();
+      currentPlayer.moveSpace(dice, board);
+      const space = board[currentPlayer.getSpace()];
+      if (space === 'go') {
+        currentPlayer.passGo();
+      } else if (space === 'taxes') {
+        currentPlayer.payTaxes();
       } else {
-        currentPlayer.payOtherPlayer(property.payout);
-        const propertyOwner = players.find(e => e.name === property.owner);
-        propertyOwner.getPaid(property.payout);
+        const property = props.checkProperties(space);
+        // console.log({property});
+        if (property.owner === null) {
+          currentPlayer.purchaseProperty(property);
+          props.assignOwner(currentPlayer.name, property.name);
+        } else {
+          currentPlayer.payOtherPlayer(property.type, property.payout);
+          const propertyOwner = players.find(e => e.name === property.owner);
+          propertyOwner.getPaid(property.type, property.payout);
+        }
       }
+
+      currentPlayer.buyHouses();
     }
 
     if (players.length-1 === current) {
@@ -43,28 +47,28 @@ const playGame = (players, props) => {
     }
 
     total++;
-    if (total === 50) {
+    if (total === 100) {
       console.log(
         'player1', {
           space: players[0].space,
           money: players[0].money,
-          propertiesRed: players[0].properties.red.owned,
-          propertiesBlue: players[0].properties.blue.owned,
-          propertiesGreen: players[0].properties.green.owned
+          // propertiesRed: players[0].properties.red.owned,
+          // propertiesBlue: players[0].properties.blue.owned,
+          // propertiesGreen: players[0].properties.green.owned
         },
         'player2', {
           space: players[1].space,
           money: players[1].money,
-          propertiesRed: players[1].properties.red.owned,
-          propertiesBlue: players[1].properties.blue.owned,
-          propertiesGreen: players[1].properties.green.owned
+          // propertiesRed: players[1].properties.red.owned,
+          // propertiesBlue: players[1].properties.blue.owned,
+          // propertiesGreen: players[1].properties.green.owned
         },
         'player3', {
           space: players[2].space,
           money: players[2].money,
-          propertiesRed: players[2].properties.red.owned,
-          propertiesBlue: players[2].properties.blue.owned,
-          propertiesGreen: players[2].properties.green.owned
+          // propertiesRed: players[2].properties.red.owned,
+          // propertiesBlue: players[2].properties.blue.owned,
+          // propertiesGreen: players[2].properties.green.owned
         },
         // props
       );
